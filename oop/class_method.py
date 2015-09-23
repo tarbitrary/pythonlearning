847f48725366d3f02e3c4768fcf2fd9fbc458684
tarbitrary <911129075@qq.com>
2015-09-23 12:51:59 +0800

    完善相关方法

--- oop/class_method.py
+++ oop/class_method.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python
 #-- coding: utf-8 --
 
+class Person(object):
+	def __init__(self, name, age):
+		self.__name = name
+		self.__age = age
+
+	def get_personinfo(self):
+		print("%s->%s"%(self.__name, self.__age))
